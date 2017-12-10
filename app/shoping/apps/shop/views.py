45a5a02a57d4b1027985dae632778045c5a3fdd4
Vinicio Valbuena <formatcomvinicio@gmail.com>
2017-12-09 22:50:13 -0500

    se elimino logs basura

--- app/shoping/apps/shop/views.py
+++ app/shoping/apps/shop/views.py
@@ -1,10 +1,7 @@
-import logging
 from django.views.generic import ListView, DetailView
 from django.views.generic import TemplateView
 from carton.cart import Cart
 from shoping.apps.product.models import Product
-
-logger = logging.getLogger(__name__)
 
 class ProductListView(ListView):
     model = Product
@@ -14,10 +11,6 @@ class ProductDetailView(DetailView):
 
 class CartShowView(TemplateView):
     template_name = 'shop/cart_list.html'
-
-    def get(self, request, *args, **kwargs):
-        logger.debug(request.session.items())
-        return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         action = request.POST.get('action')
